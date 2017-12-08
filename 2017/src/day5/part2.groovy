6d1ed6c13efd3bdb9113d307f54ca30fb8ee159e
David Nguyen <david@netis.be>
2017-12-08 17:00:16 +0100

    day 5

---
+++ 2017/src/day5/part2.groovy
@@ -0,0 +1,22 @@
+package day5
+
+
+def jump(List<Integer> input) {
+  def maze = input.collect()
+  def currentIndice = 0
+  def steps = 0
+  while (currentIndice >= 0 && currentIndice < maze.size()) {
+    def currentValue = maze[currentIndice]
+    def nextIndice = currentIndice + currentValue
+    maze[currentIndice] = currentValue + (currentValue > 2 ? -1 : 1)
+    currentIndice = nextIndice
+    steps++
+  }
+
+  steps
+}
+
+assert jump([0, 3, 0, 1, -3]) == 10
+
+def input = new File('input.txt').readLines().collect { it.trim() as int }
+println jump(input)
